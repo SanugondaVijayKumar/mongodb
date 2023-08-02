bcc06fe8a4e76df0d221b30c29d23793d275112f
SanugondaVijayKumar <myemail>
2023-08-02 17:06:12 +0530

    task 4

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById("64ca28fedf369963f2f288fc")
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name,user.email,user.cart,user._id);
       next();
     })
     .catch(err => console.log(err));
