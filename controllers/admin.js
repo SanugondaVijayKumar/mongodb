c44c08f4dcf0ca0f08090495303e1957acd637c6
SanugondaVijayKumar <myemail>
2023-08-03 09:34:55 +0530

    task 8

--- controllers/admin.js
+++ controllers/admin.js
@@ -16,7 +16,12 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product=new Product(title,imageUrl,price,description,null,req.user._id);
+  const product=new Product({
+    title:title,
+    imageUrl:imageUrl,
+    price:price,
+    description:description
+  });
     product.save().then(result => {
       // console.log(result);
       console.log('Created Product');
@@ -56,8 +61,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
   
-  const product=new Product(updatedTitle,updatedPrice,updatedDesc,updatedImageUrl,prodId);  
-    product.save().then(result => {
+  Product.findById(prodId).then(product=>{
+    product.title=updatedTitle;
+    product.imageUrl=updatedImageUrl;
+    product.price=updatedPrice;
+    product.description=updatedDesc;
+    return product.save()
+  }) 
+    .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
     })
@@ -65,7 +76,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -78,7 +89,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(()=> {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
