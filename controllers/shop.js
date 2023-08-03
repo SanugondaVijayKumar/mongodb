a75af9c23c57cdae06085eff652887e47930581c
SanugondaVijayKumar <myemail>
2023-08-03 11:39:17 +0530

    task 10

--- controllers/shop.js
+++ controllers/shop.js
@@ -44,8 +44,10 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products=user.cart.items;
           res.render('shop/cart', {
             path: '/cart',
             pageTitle: 'Your Cart',
@@ -60,6 +62,7 @@ exports.postCart = (req, res, next) => {
   Product.findById(prodId)
   .then(product=>{
     return req.user.addToCart(product);
+
   }).then(result=>{
     console.log(result);
     res.redirect('/cart');
--- models/user.js
+++ models/user.js
@@ -11,8 +11,30 @@ const userSchema=new Schema({
         type:String,
         required:true
     },
-    cart:[{productId:{type:Schema.Types.ObjectId ,ref:'Product'},quantity:{type:Number}}]
+    cart:[{productId:{type:Schema.Types.ObjectId ,ref:'Product',required:true},quantity:{type:Number,required:true}}]
 });
+
+userSchema.methods.addToCart=function(product){
+    const cartProductIndex=this.cart.items.findIndex(cp=>{
+              return cp.productId.toString()===product._id.toString();
+            });
+            let newQuantity=1;
+            const updatedCartItems=[...this.cart.items];
+        
+            if(cartProductIndex>=0){
+              newQuantity=this.cart.items[cartProductIndex].quantity+1;
+              updatedCartItems[cartProductIndex].quantity=newQuantity;
+            }else{
+              updatedCartItems.push({productId:product._id,quantity:newQuantity})
+            }
+            
+        
+            const updatedCart={
+              items:updatedCartItems
+            };
+            this.cart=updatedCart;
+            return this.save();
+}
 
 module.exports=mongoose.model('User',userSchema);
 
