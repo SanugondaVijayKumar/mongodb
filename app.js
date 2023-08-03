5c7255754f0fad8e343073c0f6afe7c05f982870
SanugondaVijayKumar <myemail>
2023-08-03 10:34:45 +0530

    task 9

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose=require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User=require('./models/user');
+const User=require('./models/user');
 
 const app = express();
 
@@ -18,21 +18,36 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById("64ca28fedf369963f2f288fc")
-//     .then(user => {
-//       req.user = new User(user.name,user.email,user.cart,user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("64cb2b603a3a00dcbfd90529")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://vijaysanugonda:3b0U8J6BVJoyyqpu@cluster0.ho2bocm.mongodb.net/shop?retryWrites=true&w=majority').then(result=>{
+mongoose.connect('mongodb+srv://vijaysanugonda:3b0U8J6BVJoyyqpu@cluster0.ho2bocm.mongodb.net/shop?retryWrites=true&w=majority')
+.then(result=>{
+  User.findOne().then(user=>{
+    if(!user){
+      const user=new User({
+        name:'vijay',
+        email:'vijaysanugonda@gmail.com',
+        cart:{
+          items:[],
+          quantity:0
+        }
+      });
+      user.save();
+    }
+  })
+  
   app.listen(3000,()=>{
     console.log('server is running on 3000');
   });
--- controllers/admin.js
+++ controllers/admin.js
@@ -20,7 +20,8 @@ exports.postAddProduct = (req, res, next) => {
     title:title,
     imageUrl:imageUrl,
     price:price,
-    description:description
+    description:description,
+    userId:req.user
   });
     product.save().then(result => {
       // console.log(result);
@@ -77,7 +78,10 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+  // .select('title price -_id')
+  // .populate('userId','name')
     .then(products => {
+      console.log(products);
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
